e39d8927c30bb4725cc993c595304b411a21b0c2
misxka <mishutkavlesu@gmail.com>
2020-10-18 19:31:49 +0300

    feat: add solution to dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let cryptoStr = '';
+    let names = [];
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        while (members[i][0] === ' ') {
+          members[i] = members[i].slice(1);
+        }
+        names.push(members[i]);
+      }
+    }
+    names.sort();
+    let name;
+    for (let i = 0; i < names.length; i++) {
+      name = names[i][0].toUpperCase();
+      names[i] = name;
+    }
+    names.sort();
+    for (let i = 0; i < names.length; i++) {
+      cryptoStr += names[i][0];
+    }
+    return cryptoStr;
+  }
+  else return false;
 };
+
+//console.log(createDreamTeam(['vcbga']));
