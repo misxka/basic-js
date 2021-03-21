b48f53c1f88161b3388e90a6879efa8d7c9038bf
misxka <mishutkavlesu@gmail.com>
2021-03-21 18:51:38 +0300

    fix: fix bug in transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -10,7 +10,7 @@ module.exports = function transform(arr) {
     }
     else if(arr[i] === sequences[1]) {
       if(typeof(arr[i - 1]) !== 'undefined') {
-        if(arr[i - 2] !== sequences[0] || (Number.isNaN(arr[i - 1]) && Number.isNaN(newArr[newArr.length - 1]))) newArr.pop();
+        if(arr[i - 2] !== sequences[0]) newArr.pop();
       }
     }
     else if(arr[i] === sequences[2]) {
@@ -18,7 +18,7 @@ module.exports = function transform(arr) {
     }
     else if(arr[i] === sequences[3]) {
       if(typeof(arr[i - 1]) !== 'undefined') {
-        if(arr[i - 2] !== sequences[0] || (Number.isNaN(arr[i - 1]) && Number.isNaN(newArr[newArr.length - 1]))) newArr.push(newArr[newArr.length - 1]);
+        if(arr[i - 2] !== sequences[0] ) newArr.push(newArr[newArr.length - 1]);
       }
     }
     else newArr.push(arr[i]);
@@ -28,24 +28,54 @@ module.exports = function transform(arr) {
 
 // console.log(transform([
 //   '--double-prev',
-//   0,
-//   '--double-prev',
-//   'ABC',
-//   '--double-prev',
-//   333,
-//   '--double-next',
-//   22,
-//   '--discard-prev',
-//   'ABC',
+//   1.233,
 //   '--discard-next',
+//   '8.963',
+//   '--double-prev',
+//   1,
+//   '--double-next',
+//   1,
+//   '--discard-prev',
 //   { '0': 'first', '1': 'second', length: 2 },
 //   '--discard-prev',
-//   'DEF',
-//   Infinity,
-//   3.14,
-//   'ABC',
+//   1.233,
+//   '--double-prev',
+//   1.233,
+//   '--discard-next',
 //   false,
-//   true,
+//   '--double-prev',
+//   22,
+//   '--double-next',
+//   { '0': 'first', '1': 'second', length: 2 },
+//   '--double-next',
 //   NaN,
-//   '--discard-prev'
+//   '--discard-next',
+//   22,
+//   '--discard-next',
+//   NaN,
+//   '--discard-prev',
+//   NaN,
+//   '--discard-next',
+//   { '0': 'first', '1': 'second', length: 2 },
+//   '--discard-next',
+//   3.14,
+//   '--double-next',
+//   true,
+//   0,
+//   false,
+//   0,
+//   22,
+//   NaN,
+//   'ABC',
+//   333,
+//   'GHI',
+//   1,
+//   1.233,
+//   'DEF',
+//   { '0': 'first', '1': 'second', length: 2 },
+//   3.14,
+//   333,
+//   false,
+//   NaN,
+//   '--discard-next'
 // ]));
