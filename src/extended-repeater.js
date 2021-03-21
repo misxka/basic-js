b4513371cc09134c5625efe953995a6bf23a1485
misxka <mishutkavlesu@gmail.com>
2021-03-21 13:54:32 +0300

    fix: fix problem with nullish coalescing operator

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -6,18 +6,18 @@ module.exports = function repeater(str, options) {
   str = String(str);
   options.addition = typeof(options.addition) !== 'undefined' ? options.addition : '';
   options.addition = String(options.addition);
-  let repeatTimes = options.additionRepeatTimes ?? 1;
+  let repeatTimes = typeof(options.additionRepeatTimes) !== 'undefined' ? options.additionRepeatTimes : 1;
   for(let i = 0; i < repeatTimes; i++) {
     arr.push(options.addition);
   }
-  options.addition = arr.join(options.additionSeparator ?? '|');
+  options.addition = arr.join(typeof(options.additionSeparator) !== 'undefined' ? options.additionSeparator : '|');
   arr = [];
   str = str + options.addition;
-  repeatTimes = options.repeatTimes ?? 1;
+  repeatTimes = typeof(options.repeatTimes) !== 'undefined' ? options.repeatTimes : 1;
   for(let i = 0; i < repeatTimes; i++) {
     arr.push(str);
   }
-  str = arr.join(options.separator ?? '+');
+  str = arr.join(typeof(options.separator) !== 'undefined' ? options.separator : '+');
   return str;
 };
   
