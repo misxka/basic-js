eab3364d72ee58c972df624bff37b65758d5c106
misxka <mishutkavlesu@gmail.com>
2021-03-21 23:12:45 +0300

    fix: fix bug in solution to simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -17,38 +17,33 @@ const chainMaker = {
       let i = 0;
       do {
         n = str.indexOf(substr);
-        // if (n > -1) {
-          str = str.slice(n + 1);
-          i++;
-        // }
+        str = str.slice(n + 1);
+        i++;
       }
       while (n > -1);
       return i + 1;
     };
 
-    const getPosition = function(str, substr, number) {
-      let n = 0;
-      let i = 0;
-      let tempStr = str;
-      while(i < number) {
-        n = n + tempStr.indexOf(substr);
-        tempStr = tempStr.slice(n + 1);
-        i++;
+    const getPosition = function getIndex(str, substr, nth) { 
+      let offset = 0;
+      for (let i = 0; i < nth; i++) {
+            offset = str.indexOf(substr, offset + 1);
       }
-      return n;
-    };
+      return offset;
+    } 
 
-    if(typeof(position) != 'number' || !Number.isInteger(position) ||  
-                position < 1 || position > getChainLength(this.chain, ')~~('))
-                 throw new Error();
+    if(typeof(position) != 'number' || !Number.isInteger(position) || position < 1 || position > getChainLength(this.chain, ')~~(')) {
+      this.chain = '';
+      throw new Error();
+    }
 
     if(position === 1) {
-      this.chain = this.chain.substring(this.chain.indexOf(')~~(') + 4, this.chain.length);
+      this.chain = this.chain.substring(this.chain.indexOf(')~~(') + 3, this.chain.length);
     }
     else {
       let firstPos = getPosition(this.chain, ')~~(', position - 1);
       let secondPos = getPosition(this.chain, ')~~(', position);
-      this.chain = this.chain.substring(0, firstPos) + this.chain.substring(secondPos + 1, this.chain.length);
+      this.chain = this.chain.substring(0, firstPos) + this.chain.substring(secondPos, this.chain.length);
     }
 
     return this;
@@ -60,11 +55,10 @@ const chainMaker = {
     return this;
   },
   finishChain() {
-    return this.chain;
+    const str = this.chain;
+    this.chain = '';
+    return str;
   }
 };
-
-// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
-// '( 3rd )~~( function() {} )'
 
 module.exports = chainMaker;
